dc2526a060788124837e868d2d53505ae5407d17
sudharsan18sep <sudharsan18sep@gmail.com>
2025-08-12 23:22:03 +0100

    first commit

---
+++ patientservice/src/main/java/com/pm/patientservice/model/Patient.java
@@ -0,0 +1,90 @@
+package com.pm.patientservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+//this is an entity class used to persist data from database
+@Entity
+public class Patient {
+    //the memebers are public so that can only be accessed through getters and setters
+    // encourages good encapsulation
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id; // id of patient
+
+    //name cannot be null for validation in persistance level
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true) //prevent duplicate email
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registeredDate;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public @NotNull String getName() {
+        return name;
+    }
+
+    public void setName(@NotNull String name) {
+        this.name = name;
+    }
+
+    public @NotNull @Email String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotNull @Email String email) {
+        this.email = email;
+    }
+
+    public @NotNull String getAddress() {
+        return address;
+    }
+
+    public void setAddress(@NotNull String address) {
+        this.address = address;
+    }
+
+    public @NotNull LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(@NotNull LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public @NotNull LocalDate getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(@NotNull LocalDate registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+
+
+
+
+
+
+}
